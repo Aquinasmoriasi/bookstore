1eaa27e491dd3804f6c8e1a8e74c86c7db35f199
unknown <aquinasmoriasi@gmail.com>
2022-08-23 14:49:38 +0300

    correct errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book, remove }) => (
-  <li id={book.id} key={book.id}>
-    <div className="bookItem">
-      <p>
-        <span>Action</span>
-        <span>{book.title}</span>
-        <span>{book.author}</span>
-      </p>
-      <div>
-        <button type="button">Comments</button>
-        <button type="button" onClick={remove}>Remove</button>
-        <button type="button">Edit</button>
+const Book = ({ book, remove }) => {
+  const { id, title, author } = book;
+  return (
+    <li id={id} key={id}>
+      <div className="bookItem">
+        <p>
+          <span>Action</span>
+          <span>{title}</span>
+          <span>{author}</span>
+        </p>
+        <div>
+          <button type="button">Comments</button>
+          <button type="button" onClick={remove}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
       </div>
-    </div>
-    <div>
-      <p>
-        <span>50%</span>
-        <span>Complete</span>
-      </p>
-    </div>
-    <div>
-      <p>
-        <span>Current Chapter</span>
-        <span>Chapter</span>
-        <button type="button">Update Progress</button>
-      </p>
-    </div>
-  </li>
-);
+      <div>
+        <p>
+          <span>50%</span>
+          <span>Complete</span>
+        </p>
+      </div>
+      <div>
+        <p>
+          <span>Current Chapter</span>
+          <span>Chapter</span>
+          <button type="button">Update Progress</button>
+        </p>
+      </div>
+    </li>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.objectOf,
@@ -37,7 +40,7 @@ Book.propTypes = {
 };
 
 Book.defaultProps = {
-  book: PropTypes.objectOf,
+  book: {},
 };
 
 export default Book;
