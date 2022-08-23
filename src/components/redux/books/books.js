2b5ba752cc605f130bb70ba59080b55f04f0574c
unknown <aquinasmoriasi@gmail.com>
2022-08-23 17:54:41 +0300

    add hooks

--- src/components/redux/books/books.js
+++ src/components/redux/books/books.js
@@ -1,7 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [{
+  title: 'Uncle Tom\'s Cabin',
+  author: 'Harriet Beacher Stowe',
+  id: uuidv4(),
+},
+{
+  title: 'Cannot Hurt Me ',
+  author: 'David Goggins',
+  id: uuidv4(),
+},
+{
+  title: 'When We Met',
+  author: 'Danielle Steele',
+  id: uuidv4(),
+},
+{
+  title: 'Gifted Hands',
+  author: 'Ben Carson',
+  id: uuidv4(),
+}];
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
